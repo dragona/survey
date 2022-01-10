5cb49b83dbae8d71bfd7297c4124675751cda209
Feno Heriniaina <studio@Fenos-MacBook-Pro-2.local>
2022-01-10 13:50:35 +0800

    Kotlin

---
+++ app/src/main/java/mg/x261/survey01/MainActivity.kt
@@ -0,0 +1,11 @@
+package mg.x261.survey01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome)
+    }
+}
